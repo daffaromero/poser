ea8ddec27dbf0fc31eea4713efcdbc0fb14e6beb
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-10-14 21:19:58 +0700

    docker: postgres + pgadmin set up and running

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InvoiceModule } from './invoice/invoice.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'invoice_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    InvoiceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
